4bd22d9ae5468b7ef5aebb51ec9ffb79abda3683
QTudor321 <tudorovesea333@gmail.com>
2026-03-03 20:41:54 +0200

    JIRA-11: Implement testing architecture in backend project files

---
+++ src/test/java/com/spring/hotelreservationsystem/controllers/RoomControllerTest.java
@@ -0,0 +1,12 @@
+package com.hotel.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest
+class RoomControllerTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
