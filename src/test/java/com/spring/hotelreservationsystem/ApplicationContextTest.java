92c37043b8eb4ea31cd15479973ab4c927ae5a4f
QTudor321 <tudorovesea333@gmail.com>
2026-03-03 20:55:56 +0200

    JIRA-12: Update pom plugins and dependencies, add unit testing plugin

---
+++ src/test/java/com/spring/hotelreservationsystem/ApplicationContextTest.java
@@ -0,0 +1,11 @@
+package org.example.hotelreservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationContextTest {
+    @Test
+    void contextLoads() {
+    }
+}
