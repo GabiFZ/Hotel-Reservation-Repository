d4675114cdd27207a6c63dcdaf0cbf614700b12b
Filip Dumitru <filip030804@gmail.com>
2026-03-07 17:25:54 +0200

    SCRUM-28 Fix RoomIntegrationTest Spring Boot configuration

--- src/test/java/com/spring/hotelreservationsystem/integration/RoomIntegrationTest.java
+++ src/test/java/com/spring/hotelreservationsystem/integration/RoomIntegrationTest.java
@@ -1,12 +1,13 @@
-package com.hotel.integration;
+package com.spring.hotelreservationsystem.integration;
 
+import com.spring.hotelreservationsystem.HotelReservationSystemApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = HotelReservationSystemApplication.class)
 class RoomIntegrationTest {
 
     @Test
-    void fullContextLoads() {
+    void contextLoads() {
     }
 }
